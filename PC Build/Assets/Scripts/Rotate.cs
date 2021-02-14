64d616643e6a81282be7790004372832ff55b6d5
Andres Largo <felipeflogxd@gmail.com>
2021-02-14 01:44:23 -0500

    Laser

---
+++ PC Build/Assets/Scripts/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    int velocidade = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.left * velocidade * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.left * velocidade * Time.deltaTime);
+
+        }
+    }
+}
